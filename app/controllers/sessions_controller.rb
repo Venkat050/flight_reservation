8708ffcf63409923097536c5b425a78c7c2ce1b5
venkat <venkatkumar.hari1976@gmail.com>
2020-08-24 11:06:14 +0530

    Validation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,20 @@
 class SessionsController < ApplicationController
 
     def new
+        @user = User.new
         if session[:user_id] != nil
             redirect_to root_path
         end
     end
 
     def create
-        user = User.find_by(email: params[:session][:email].downcase)
-        if user && user.authenticate(params[:session][:password])
+        @user = User.find_by(email: params[:session][:email].downcase)
+        if @user && @user.authenticate(params[:session][:password])
             session[:user_id] = user.id
             redirect_to root_path
         else
             flash.now[:alert] = "Please check your email/password"
+            render 'new'
         end
     end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,10 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save 
-          flash[:notice] = "Article created succesfully!"
+          flash[:notice] = "Account created succesfully!"
           redirect_to root_path
+        else
+            render 'new'
         end
     end
 
